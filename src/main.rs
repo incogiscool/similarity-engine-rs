c4dc5ffde26987fe2ac2431226be575e83948917
incogiscool <adam546944@gmail.com>
2023-09-05 18:37:35 +0400

    formatting + logs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use std::time::{Instant, Duration};
+use std::time::Instant;
 
 #[derive(Debug)]
 struct Item {
     rating: Vec<i32>,
     title: String,
     description: String, // Corrected the field name to "description"
-    id: i32
+    id: i32,
 }
 
 #[derive(Debug)]
 struct Key {
     title: String,
-    weight: i32
+    weight: i32,
 }
 
 #[derive(Debug)]
@@ -24,7 +24,8 @@ struct Similarity {
 struct SimilarityArrayObject<'a> {
     id: i32,
     against: &'a Item,
-    similarity: f64
+    similarity: f64,
+    title: &'a String,
 }
 
 impl Similarity {
@@ -41,9 +42,12 @@ impl Similarity {
             panic!("All Items ratings length must be equal.")
         };
 
-        let dot_product = item_1.rating.iter()
-        .zip(item_2.rating.iter())
-        .map(|(&x, &y)| x * y).sum();
+        let dot_product = item_1
+            .rating
+            .iter()
+            .zip(item_2.rating.iter())
+            .map(|(&x, &y)| x * y)
+            .sum();
 
         return dot_product;
     }
@@ -51,9 +55,8 @@ impl Similarity {
     pub fn magnitude(&self, item: &Item) -> f64 {
         let values_squared_added: i32 = item.rating.iter().map(|rating| rating * rating).sum();
 
-        return f64::from(values_squared_added).sqrt()
+        return f64::from(values_squared_added).sqrt();
     }
-
 
     pub fn cosine_similarity(&self, item_1: &Item, item_2: &Item) -> f64 {
         let dot_product = self.dot_product(item_1, item_2);
@@ -61,14 +64,18 @@ impl Similarity {
         let magnitude_2 = self.magnitude(item_2);
 
         if magnitude_1 == 0.0 || magnitude_2 == 0.0 {
-            return 0.0
+            return 0.0;
         };
 
-        return f64::from(dot_product) / (magnitude_1 * magnitude_2)
+        return f64::from(dot_product) / (magnitude_1 * magnitude_2);
     }
 
     pub fn get_similar(&self, item_id: i32) -> Vec<SimilarityArrayObject> {
-        let item = self.items.iter().find(|item| item.id == item_id).expect("Couldn't find item");
+        let item = self
+            .items
+            .iter()
+            .find(|item| item.id == item_id)
+            .expect("Couldn't find item");
 
         let mut all_items_calculated: Vec<SimilarityArrayObject> = vec![];
 
@@ -80,24 +87,32 @@ impl Similarity {
             let value = self.cosine_similarity(item, &current_item);
 
             all_items_calculated.push(SimilarityArrayObject {
-                id: item.id,
+                id: current_item.id,
                 similarity: value,
-                against: current_item
+                against: current_item,
+                title: &current_item.title,
             });
         }
 
         // Sort the similarities in descending order
-        all_items_calculated.sort_by(|a, b| b.similarity.partial_cmp(&a.similarity).unwrap_or(std::cmp::Ordering::Equal));
+        all_items_calculated.sort_by(|a, b| {
+            b.similarity
+                .partial_cmp(&a.similarity)
+                .unwrap_or(std::cmp::Ordering::Equal)
+        });
 
         // Take the top 5 items
-        let top_5_similar = all_items_calculated.iter().take(5).cloned().collect::<Vec<_>>();
+        let top_5_similar = all_items_calculated
+            .iter()
+            .take(5)
+            .cloned()
+            .collect::<Vec<_>>();
 
         return top_5_similar;
-
     }
 }
 
-fn main() {
+fn init_test_similarity_engine() -> Similarity {
     let mut engine = Similarity {
         keys: vec![],
         items: vec![],
@@ -105,61 +120,115 @@ fn main() {
 
     engine.create_key(Key {
         title: String::from("Comedy"),
-        weight: 3
+        weight: 3,
     });
 
     engine.create_key(Key {
         title: String::from("Action"),
-        weight: 2
+        weight: 2,
     });
 
     engine.create_item(Item {
         title: String::from("Brooklyn 99"),
         rating: vec![10, 4],
         description: String::from("Its a show..."),
-        id: 1
+        id: 1,
     });
 
     engine.create_item(Item {
         title: String::from("Rush Hour 2"),
         rating: vec![10, 7],
         description: String::from("Its a movie..."),
-        id: 2
+        id: 2,
     });
 
     engine.create_item(Item {
         title: String::from("Rush Hour 3"),
         rating: vec![8, 7],
         description: String::from("Its a  nother movie..."),
-        id: 3
+        id: 3,
     });
 
     engine.create_item(Item {
         title: String::from("my custom action movie"),
         rating: vec![4, 10],
         description: String::from("Its a action movie..."),
-        id: 4
+        id: 4,
     });
 
     engine.create_item(Item {
         title: String::from("John Wick"),
         rating: vec![2, 9],
         description: String::from("Its another movie..."),
-        id: 5
+        id: 5,
     });
+
+    engine.create_item(Item {
+        rating: vec![8, 7],
+        title: String::from("Central Intelligence"),
+        description: String::from("movi"),
+        id: 6,
+    });
+
+    engine.create_item(Item {
+        title: String::from("The Hangover"),
+        rating: vec![9, 2],
+        description: String::from("It's a comedy..."),
+        id: 7,
+    });
+
+    engine.create_item(Item {
+        title: String::from("Die Hard"),
+        rating: vec![3, 10],
+        description: String::from("It's an action movie..."),
+        id: 8,
+    });
+
+    engine.create_item(Item {
+        title: String::from("Forrest Gump"),
+        rating: vec![9, 2],
+        description: String::from("A classic..."),
+        id: 9,
+    });
+
+    engine.create_item(Item {
+        title: String::from("The Matrix"),
+        rating: vec![4, 10],
+        description: String::from("Sci-fi action..."),
+        id: 10,
+    });
+
+    return engine;
+}
+
+fn main() {
+    let engine = init_test_similarity_engine();
+
+    let id = 5;
+
+    let id_item = engine
+        .items
+        .iter()
+        .find(|item| item.id == id)
+        .expect("Invalid id");
+
+    println!("Finding top 5 for {}...\n", id_item.title);
 
     let start_time = Instant::now();
 
-    let similar = engine.get_similar(5);
-    println!("{:?}", similar);
+    let similar = engine.get_similar(id);
 
-        // End measuring time
-        let end_time = Instant::now();
+    let end_time = Instant::now();
+    let elapsed_time = end_time - start_time;
 
-        // Calculate the elapsed time
-        let elapsed_time = end_time - start_time;
-    
-        // Print the elapsed time
-        println!("Elapsed time: {:?}s", elapsed_time.as_secs_f32());
+    similar.iter().enumerate().for_each(|(index, item)| {
+        println!(
+            "Item: {}\nPlacement: {}\nScore: {}%\n",
+            item.title,
+            index + 1,
+            (item.similarity * 100.00).round()
+        )
+    });
 
+    println!("Execution time: {:?}", elapsed_time);
 }
